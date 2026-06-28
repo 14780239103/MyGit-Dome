df2059b3661bad8d63fa90b3c430c9ece0027bbf
yangjun <719405852@qq.com>
2026-06-28 12:54:16 +0800

    test分支修改3 增加hello test

--- dome-moude-1/src/main/java/com/duyi/MainTest.java
+++ dome-moude-1/src/main/java/com/duyi/MainTest.java
@@ -3,5 +3,8 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("今天天气很凉爽 我很喜欢这种天气");
+        System.out.println("hello test分支");
+
+
     }
 }
